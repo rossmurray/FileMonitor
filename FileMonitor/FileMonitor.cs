20dc5a5bf307b7f70dce78a027bae65388227f0f
rossmurray <redcancel@gmail.com>
2013-05-01 05:29:52 -0500

    Initial commit, not functional.

---
+++ FileMonitor/FileMonitor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace FileMonitor
+{
+	/// <summary>
+	/// Watches a file for changes, raising an event when a file has finished changing.
+	/// </summary>
+	public class FileMonitor : IDisposable
+	{
+		public event EventHandler<FileChangedEventArgs> FileChanged;
+		private FileSystemWatcher watcher;
+		private object startStopLock;
+		private bool disposed;
+
+		public FileMonitor()
+		{
+			this.startStopLock = new object();
+			this.disposed = false;
+			this.watcher = new FileSystemWatcher();
+			this.watcher.Changed += new FileSystemEventHandler(FileChangedHandler);
+			this.watcher.NotifyFilter = NotifyFilters.LastWrite;
+		}
+
+		public void Watch(FileInfo file)
+		{
+			if (disposed) { throw new ObjectDisposedException(this.GetType().Name); }
+			if (file == null) { throw new ArgumentNullException("file"); }
+			lock (this.startStopLock)
+			{
+				this.watcher.EnableRaisingEvents = false;
+				this.watcher.Path = file.DirectoryName;
+				this.watcher.Filter = file.Name;
+				this.watcher.EnableRaisingEvents = true;
+			}
+		}
+
+		public void StopWatching()
+		{
+			if (disposed) { throw new ObjectDisposedException(this.GetType().Name); }
+			lock (this.startStopLock)
+			{
+				this.watcher.EnableRaisingEvents = false;
+			}
+		}
+
+		private void FileChangedHandler(object sender, FileSystemEventArgs e)
+		{
+			if (disposed) { return; }
+			throw new NotImplementedException();
+			//throw this event in a queue (another class) whose job is trying to open the file to determine it's done changing.
+			//inform event subscribers when the file is available.
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			lock (this.startStopLock)
+			{
+				this.watcher.Dispose();
+			}
+			this.disposed = true;
+		}
+	}
+}
---
+++ FileMonitor/FileUtil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace FileMonitor
+{
+	public static class FileUtil
+	{
+		/// <summary>
+		/// Attempts to open a file repeatedly until the timeout is reached.
+		/// </summary>
+		/// <remarks>This method is not recommended for general use.
+		/// Just because this method was able to open the file does not mean the file remains uncontested.
+		/// This method simply indicates a probability that a file that was in use has ceased being in use, assuming the file only had one accessor.</remarks>
+		/// <returns>Whether or not the file was successfully opened.</returns>
+		public static bool WaitForUnlock(FileInfo file, int timeoutMs)
+		{
+			var begin = DateTime.UtcNow;
+			while ((DateTime.UtcNow - begin).TotalMilliseconds < timeoutMs)
+			{
+				try
+				{
+					using (file.Open(FileMode.Open, FileAccess.Read, FileShare.None))
+					{
+						return true;
+					}
+				}
+				catch (IOException)
+				{
+					//it's feasible to examine HResult codes here and throw on codes other than sharing violations,
+					//but there are a lot of different reasons the file would be in used by something else
+					//such as locking only a portion of a file, open by another process, etc.
+					Thread.Sleep(300);
+				}
+			}
+			return false;
+		}
+	}
+}
